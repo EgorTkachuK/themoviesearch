e3e8d37c40d3d04fff850467e762172b6d4729d3
EgorTkachuK <134726720+EgorTkachuK@users.noreply.github.com>
2026-02-01 10:54:35 +0100

    finale++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Movies from './Movies';
 import MovieDetails from './MovieDetails';
